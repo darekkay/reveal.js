961e9412760416bb26666240f3456d7273684a20
Darek Kay <darekkay@gmail.com>
2020-05-14 23:28:19 +0200

    Add theme toggle plugin

--- gruntfile.js
+++ gruntfile.js
@@ -74,7 +74,7 @@ module.exports = grunt => {
 					{src: ['css/print/pdf.css'], dest: 'build/', expand: true},
           {src: ['css/theme/*.min.css'], dest: 'build/', expand: true},
           {
-            src: ['plugin/highlight/*', 'plugin/notes/*', 'plugin/markdown/*',  'plugin/schedule/*'],
+            src: ['plugin/highlight/*', 'plugin/notes/*', 'plugin/markdown/*',  'plugin/schedule/*', 'plugin/theme-toggle/*'],
             dest: 'build/', expand: true
           },
         ],
